38104f7d7331c5f8106731001ed7ca559114e961
LEEHOYOUNG <flghdud1@gmail.com>
2023-05-01 16:32:28 +0900

    Apply #1
    씨부랄

--- src/main/java/com/fastcampus/config/Config.java
+++ src/main/java/com/fastcampus/config/Config.java
@@ -2,7 +2,9 @@ package com.fastcampus.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.fastcampus")
 @Configuration
 public class Config {
